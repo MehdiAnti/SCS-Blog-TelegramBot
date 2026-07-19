f55263f0dcd4addc3a87742c2b5f8257d4fe7dc1
Mehdi <107349844+MehdiAnti@users.noreply.github.com>
2026-07-19 16:51:54 +0330

    Update blogger.py
    - typo

--- app/blogger.py
+++ app/blogger.py
@@ -161,7 +161,7 @@ def _get_teaser(text, limit=400):
         return text
 
     return (
-        text[:limit].rstrip(" ", 1)[0]
+        text[:limit].rsplit(" ", 1)[0]
         + "..."
     )
     
