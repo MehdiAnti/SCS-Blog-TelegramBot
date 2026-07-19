152dd1531ab2a4c633e099f93f4641a2c32de413
Mehdi <107349844+MehdiAnti@users.noreply.github.com>
2026-07-19 13:49:02 +0330

    Create status.py

---
+++ app/status.py
@@ -0,0 +1,16 @@
+LAST_STATUS = {
+    "rss": 0.0,
+    "article": 0.0,
+    "clean": 0.0,
+    "telegram": 0.0,
+    "kv": 0.0,
+    "total": 0.0,
+
+    "last_check": "",
+    "last_result": "",
+
+    "latest_title": "",
+    "latest_url": "",
+
+    "last_error": "",
+}
